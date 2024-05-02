b6a64d7c2ffc57fde360eba69328858c3eb3eafa
juan pablo claros <juan_pablo_claros@hotmail.com>
2024-05-02 18:19:11 +0100

    Golder master created

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,28 @@
 import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import {Item, GildedRose} from "./main.js"
 
 describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+  it("works", () => {
+    const name = [
+      'Aged Brie',
+      'Backstage passes to a TAFKAL80ETC concert',
+      'Sulfuras, Hand of Ragnaros'
+    ]
 
-    expect(result).toBe(true)
+    const listItems: Item[] = []
+
+    name.forEach((name) => {
+      for (let sellIn = -2; sellIn < 12; sellIn++) {
+        for (let quality = -1; quality < 51; quality++) {
+          const item = new Item(name,sellIn,quality)
+
+          listItems.push(item)
+          
+          const gilded = new GildedRose(listItems)
+
+          expect(gilded.updateQuality()).toMatchSnapshot()
+        }
+      }
+    })
   })
 })
