f79a84dfcc8037aedc9293480daa6c59b349cbef
juan pablo claros <juan_pablo_claros@hotmail.com>
2024-05-02 18:33:20 +0100

    Remove if statement

--- src/main.ts
+++ src/main.ts
@@ -26,16 +26,14 @@ export class GildedRose {
       } else if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
-          if (true) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
+          if (this.items[i].sellIn < 11) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
+          }
+          if (this.items[i].sellIn < 6) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1
             }
           }
         }
